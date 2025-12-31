cf54d7ca1bc898412151644e56921a343aa1fd12
Hagen10 <simon_hagerup@hotmail.com>
2025-12-31 17:02:49 +0100

    SSR rendering and transferstate for api request to ftdata

--- src/app/apiservice.ts
+++ src/app/apiservice.ts
@@ -1,14 +1,38 @@
+import { isPlatformServer } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { inject, Injectable, makeStateKey, PLATFORM_ID, TransferState } from '@angular/core';
+import { Observable, of, tap } from 'rxjs';
+
+export const POLITICIAN_STATE_KEY = makeStateKey<any>('politicians');
 
 @Injectable({
   providedIn: 'root',
 })
 export class Apiservice {
-  httpclient = inject(HttpClient);
+  private readonly URL = 'http://localhost:8080/api/politicians';
+  private httpclient = inject(HttpClient);
+  private transferState = inject(TransferState);
+  private platformId = inject(PLATFORM_ID);
 
   getData(): Observable<any> {
-    return this.httpclient.get('http://localhost:8080/api/politicians');
+    // If running in the browser and state exists → use it
+    if (
+      !isPlatformServer(this.platformId) &&
+      this.transferState.hasKey(POLITICIAN_STATE_KEY)
+    ) {
+      const data = this.transferState.get(POLITICIAN_STATE_KEY, null);
+      this.transferState.remove(POLITICIAN_STATE_KEY);
+      return of(data);
+    }
+
+    // Otherwise fetch from API
+    return this.httpclient.get(this.URL).pipe(
+      tap(data => {
+        // 3️⃣ Store only during SSR
+        if (isPlatformServer(this.platformId)) {
+          this.transferState.set(POLITICIAN_STATE_KEY, data);
+        }
+      })
+    );
   }
 }
